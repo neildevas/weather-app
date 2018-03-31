0da4b835086905c3ea92599c05fbedf90c396969
Neil Devas <neil.devas97@gmail.com>
2018-03-30 21:25:48 -0400

    make static call to api

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
+// const request = require('request');
+// const yargs = require('yargs');
+// const geocode = require('./geocode/geocode');
+// const argv = yargs
+// //346e1491510ebc45066902c8e57a928b
+// .options({
+//     a: {
+//         demand: true,
+//         alias: 'Address',
+//         describe: 'Address to fetch weather for',
+//         string: true
+//     }
+// })
+// .help()
+// .alias('help', 'h')
+// .argv;
+
+// geocode.geocodeAddress(argv.Address, (errorMessage, results) => {
+//     if (errorMessage) {
+//         console.log(errorMessage);
+//     }
+//     else {
+//         console.log(JSON.stringify(results, undefined, 2));
+//     }
+// });
+
 const request = require('request');
-const yargs = require('yargs');
-const geocode = require('./geocode/geocode');
-const argv = yargs
-.options({
-    a: {
-        demand: true,
-        alias: 'Address',
-        describe: 'Address to fetch weather for',
-        string: true
+
+request({
+    url: 'https://api.darksky.net/forecast/346e1491510ebc45066902c8e57a928b/poop,-73.6737404',
+    json: true
+}, (error, response, body) => {
+    if (error) {
+        console.log('Unable to connect to Forecast.io servers');
     }
-})
-.help()
-.alias('help', 'h')
-.argv;
-
-geocode.geocodeAddress(argv.Address);
-
+    else if (response.statusCode === 400) {
+        console.log('Unable to fetch weather');
+    }
+    else if (!error && response.statusCode === 200) {
+        console.log(body.currently.temperature);
+    }
+    else {
+        console.log('Unable to fetch weather');
+    }
+    
+});
--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,21 +1,23 @@
 const request = require('request');
 
-geocodeAddress = (address) => {
+geocodeAddress = (address, callback) => {
     const encodedAddress = encodeURIComponent(address);
     request({
         url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyBwvYsUAA28EpS6QXy3c_iTUazowdK0L6Y`,
         json: true
     }, (error, response, body) => {
         if (error) {
-            console.log('Error connecting to Google servers');
+            callback('Error connecting to Google servers');
         }
         else if (body.status === 'ZERO_RESULTS') {
-            console.log('Unable to find address');
+            callback('Unable to find address');
         }
         else if (body.status === 'OK'){
-            console.log(`Address: ${body.results[0].formatted_address}`);
-            console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-            console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
+            callback(undefined, {
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng,
+            });
         }
         
     })
